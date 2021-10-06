e41198216803b453cba53ddef5d6e297bc1b4ff6
Naga Mallesh <89628161+Mallesh511@users.noreply.github.com>
2021-10-06 17:02:12 +0530

    Update App.java

--- src/main/java/com/ibm/app/JavaApp/App.java
+++ src/main/java/com/ibm/app/JavaApp/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is the JAVA DEMO application." );
-        System.out.println( "Enabled web hook." );
+        System.out.println( "Hello World!" );
     }
 }
