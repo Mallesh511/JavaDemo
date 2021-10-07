6ce0a3fc75a0039d2fd44e463fb62a00cd0c1727
Naga Mallesh <89628161+Mallesh511@users.noreply.github.com>
2021-10-07 12:55:18 +0530

    Update App.java

--- src/main/java/com/ibm/app/JavaApp/App.java
+++ src/main/java/com/ibm/app/JavaApp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Cloud training. Webhooks testing with https" );
+        System.out.println( "Hello World! Cloud training. Webhooks tested with http" );
     }
 }
