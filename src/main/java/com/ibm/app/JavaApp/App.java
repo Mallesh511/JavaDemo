2707910937124095c4b10a92b6625493fd84e41b
Naga Mallesh <89628161+Mallesh511@users.noreply.github.com>
2021-10-07 12:35:41 +0530

    Update App.java

--- src/main/java/com/ibm/app/JavaApp/App.java
+++ src/main/java/com/ibm/app/JavaApp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Cloud training. Webhooks" );
+        System.out.println( "Hello World! Cloud training. Webhooks integrated" );
     }
 }
